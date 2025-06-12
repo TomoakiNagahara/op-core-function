f882927a96f73e4c679dd13df33f13754b427ae1
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-12 17:05:58 +0900

    New: GetErrorConstName.php

---
+++ GetErrorConstName.php
@@ -0,0 +1,8 @@
+<?php
+/**	op-core-function:/GetErrorConstName.php
+ *
+ * @created    2025-06-11
+ * @package    op-core-function
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
