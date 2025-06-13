bdff6e3dcbad18bfc3fa72007595e8f27f368ebb
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-13 18:25:28 +0900

    New: OP.php

---
+++ OP.php
@@ -0,0 +1,10 @@
+<?php
+/**	op-core-function:/OP.php
+ *
+ * @created    2022-10-05
+ * @moved      2025-06-13
+ * @version    1.0
+ * @package    op-core-function
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
