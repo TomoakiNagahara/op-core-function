aabca2586d378ceea536c1e53e1d67060734735d
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-13 18:27:55 +0900

    Add: OP.php | OP() : OP\OP

--- OP.php
+++ OP.php
@@ -8,3 +8,23 @@
  * @author     Tomoaki Nagahara
  * @copyright  Tomoaki Nagahara All right reserved.
  */
+
+/**	Return instantiated OP instance.
+ *
+ * @created    2022-10-05
+ * @return     OP\OP
+ */
+function OP()
+{
+	//	Instantiated OP instance.
+	static $_OP;
+
+	//	OP class is not loaded yet.
+	if(!$_OP ){
+		//	Instantiate \OP\OP class.
+		$_OP = new \OP\OP();
+	}
+
+	//	Return instantiated OP instance.
+	return $_OP;
+}
