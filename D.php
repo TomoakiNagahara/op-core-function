09d9046fea70f6f603ad6cc1a3a24ab58083ae83
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-14 21:16:21 +0900

    Add: D.php | PHPDoc

---
+++ D.php
@@ -0,0 +1,17 @@
+<?php
+/**	op-core-function:/D.php
+ *
+ * The "D" is a being that transcends namespaces.
+ * You just call "D".
+ *
+ * @genesis    2004  dump.inc.php
+ * @porting    2011  op-core-4:/OnePiece::Dump()
+ * @separate   2012  op-core-4:/Dump.class.php
+ * @rebirthd   2016  Separate to D() and op-unit-dump
+ * @separate   2019-02-21  op-core-5:/functions.php --> op-core-5:/function/D.php
+ * @moved      2025-06-14  op-core-function:/D.php
+ * @version    1.0
+ * @package    op-core-function
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
