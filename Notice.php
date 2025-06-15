e9ba064623a3b0a859dabd72b6281968bde5b7cc
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-15 14:51:41 +0900

    Add: Notice.php | PHPDoc

---
+++ Notice.php
@@ -0,0 +1,10 @@
+<?php
+/**	op-core-function:/Notice.php
+ *
+ * @created    2020-10-07
+ * @copied     2025-06-11
+ * @package    op-core-function
+ * @version    1.0
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
