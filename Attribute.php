a362ee7b1f39ce30fe0fdf033df9b86b318bd853
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-16 16:51:51 +0900

    Add: Attribute.php | OP\Attribute( string $attr )

--- Attribute.php
+++ Attribute.php
@@ -12,3 +12,55 @@
  *
  */
 namespace OP;
+
+/**	Parse html tag attribute from string to array.
+ *
+ * @param  string $attr
+ * @return array  $result
+ */
+function Attribute(string $attr)
+{
+	//	...
+	$key = 'tag';
+	$result = null;
+
+	//	...
+	for($i=0, $len=strlen($attr); $i<$len; $i++){
+		//	...
+		switch( $attr[$i] ){
+			case '.':
+				$key = 'class';
+				if(!empty($result[$key]) ){
+					$result[$key] .= ' ';
+				}
+				continue 2;
+
+			case '#':
+				$key = 'id';
+				continue 2;
+
+			case '?':
+				if( $result['tag'] === 'a' ){
+					$key = 'href';
+				}
+				break;
+
+			case ' ':
+				continue 2;
+
+			default:
+		}
+
+		//	...
+		if( empty($result[$key]) ){
+			$result[$key] = '';
+		}
+
+		//	...
+		$result[$key] .= $attr[$i];
+	}
+
+	//	...
+	require_once(__DIR__.'/Encode.php');
+	return Encode($result);
+}
