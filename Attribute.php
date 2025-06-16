ec043b9c1e7090c92b9eb1b1b089250257f88813
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-16 16:51:12 +0900

    Add: Attribute.php | PHPDoc

---
+++ Attribute.php
@@ -0,0 +1,9 @@
+<?php
+/**	op-core-function:/Attribute.php
+ *
+ * @moved      2025-06-16
+ * @version    1.0
+ * @package    op-core-function
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
