9751315e94e95d24a80716e6cecdbcff622011d7
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-16 16:49:26 +0900

    Add: Html.php | PHPDoc

---
+++ Html.php
@@ -0,0 +1,9 @@
+<?php
+/**	op-core-function:/Html.php
+ *
+ * @moved      2025-06-16
+ * @version    1.0
+ * @package    op-core-function
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
