a09ae14e02cb9d4c1a487183f958d898318e1fa2
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-16 16:48:25 +0900

    Add: Decode.php | OP\Decode( $value, $charset='utf-8')

--- Decode.php
+++ Decode.php
@@ -12,3 +12,37 @@
  *
  */
 namespace OP;
+
+/**	Decode can decode nested array transparent.
+ *
+ * @param  mixed  $value
+ * @param  string $charset
+ * @return string $var
+ */
+function Decode($value, $charset='utf-8')
+{
+	//	...
+	switch( gettype($value) ){
+		//	...
+		case 'string':
+			$value = html_entity_decode($value, ENT_QUOTES, $charset);
+			break;
+
+		//	...
+		case 'array':
+			$result = [];
+			foreach( $value as $key => $val ){
+				$key = is_string($key) ? Decode($key, $charset): $key;
+				$val = Decode($val, $charset);
+				$result[$key] = $val;
+			}
+			$value = $result;
+			break;
+
+		//	...
+		default:
+	}
+
+	//	...
+	return $value;
+}
