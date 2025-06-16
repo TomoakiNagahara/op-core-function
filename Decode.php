d3666ce6241a0e78d6b9957b0cf5bf46aef2e4d2
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-16 16:47:29 +0900

    Add: Decode.php | PHPDoc

---
+++ Decode.php
@@ -0,0 +1,9 @@
+<?php
+/**	op-core-function:/Decode.php
+ *
+ * @created    2025-06-16
+ * @version    1.0
+ * @package    op-core-function
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
