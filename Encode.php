91502104e84f653a9489cd620e4498b09dbb7d42
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-16 16:45:15 +0900

    Add: Encode.php | PHPDoc

---
+++ Encode.php
@@ -0,0 +1,9 @@
+<?php
+/**	op-core-function:/Encode.php
+ *
+ * @created    2019-12-20
+ * @version    1.0
+ * @package    op-core-function
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
