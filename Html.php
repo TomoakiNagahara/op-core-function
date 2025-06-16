df5115c943aacbd2b7f6e3db90795b2005472b47
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-16 16:50:09 +0900

    Add: Html.php | OP\Html( $string, $attr=null, $escape=true)

--- Html.php
+++ Html.php
@@ -12,3 +12,56 @@
  *
  */
 namespace OP;
+
+/**	Display HTML.
+ *
+ * <pre>
+ * Html('message', 'span #id .class');
+ * </pre>
+ *
+ * @param	 string		 $string
+ * @param	 string		 $config
+ * @param	 boolean	 $escape tag and quote
+ */
+function Html($string, $attr=null, $escape=true)
+{
+	//	Escape tag and quote.
+	if( $escape ){
+		require_once(__DIR__.'/Encode.php');
+		$string = Encode($string);
+	}
+
+	//	...
+	if( $attr ){
+		require_once(__DIR__.'/Attribute.php');
+		$attr = Attribute($attr);
+	}
+
+	//	...
+	$tag = $id = $class = null;
+	foreach( ['tag','id','class'] as $key ){
+		${$key} = $attr[$key] ?? null;
+	}
+
+	//	...
+	if( empty($tag) ){
+		$tag = 'div';
+	}
+
+	//	...
+	if( $tag === 'a' ){
+		$href = $attr['href'] ?? $string;
+	}
+
+	//	...
+	$attr = $id    ? " id='$id'"      :'';
+	$attr.= $class ? " class='$class'":'';
+
+	//	...
+	if( $tag === 'a' ){
+		$attr = ' href="' . $href . '"';
+		printf('<%s%s>%s</%s>'.PHP_EOL, $tag, $attr, $string, $tag);
+	}else{
+		printf('<%s%s>%s</%s>'.PHP_EOL, $tag, $attr, $string, $tag);
+	}
+}
