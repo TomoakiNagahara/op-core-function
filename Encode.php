740ae77f23850567cd86c43f4e647b41604156ea
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-16 16:46:17 +0900

    Add: Encode.php | OP\Encode( $value, $charset='utf-8')

--- Encode.php
+++ Encode.php
@@ -12,3 +12,43 @@
  *
  */
 namespace OP;
+
+/**	Encode mixed value.
+ *
+ * @created
+ * @moved     2019-12-20   From functions.php
+ * @param     mixed        $var
+ * @param     string       $charset
+ * @return    mixed        $var
+ */
+function Encode($value, $charset='utf-8')
+{
+	//	...
+	switch( gettype($value) ){
+		case 'string':
+			$value = str_replace("\0", '\0', $value);
+			return htmlentities($value, ENT_QUOTES, $charset, false);
+
+		case 'array':
+			$result = [];
+			foreach( $value as $key => $val ){
+				$key = is_string($key) ? Encode($key, $charset): $key;
+				$val = Encode($val, $charset);
+				$result[$key] = $val;
+			}
+			$value = $result;
+			break;
+
+		case 'object':
+			/*
+			D("Objects are not yet supported.");
+			*/
+			$value = get_class($value);
+			break;
+
+		default:
+	}
+
+	//	...
+	return $value;
+}
