b57364dca09070f4f0836cabc68c193b38f43a4d
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-21 15:50:06 +0900

    New: Timestamp.php

---
+++ Timestamp.php
@@ -0,0 +1,55 @@
+<?php
+/**	op-core-function:/Timestamp.php
+ *
+ * @created    2023-03-29
+ * @version    1.0
+ * @package    op-core-function
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	Get local timezone timestamp.
+ *
+ * <pre>
+ * // Local time timestamp
+ * $local_timestamp = \OP\Timestamp();
+ *
+ * // UTC time timestamp
+ * $utc       = \OP\Timestamp(true);
+ *
+ * // 1 month ago timestamp
+ * $offset    = \OP\Timestamp(false, '-1 month');
+ * </pre>
+ *
+ * @created  2019-09-24
+ * @moved    2023-03-29  OP\Env::Timestamp()
+ * @param    boolean     $utc
+ * @param    string      $offset
+ * @return   string      $timestamp
+ */
+function Timestamp(?bool $utc=false, $offset=null):string
+{
+	//	...
+	require_once(__DIR__.'/Time.php');
+
+	//	...
+	$time = Time($utc);
+
+	//	...
+	if( $offset ){
+		$time = strtotime($offset, $time);
+	}
+
+	//	...
+	return date(_OP_DATE_TIME_, $time);
+}
