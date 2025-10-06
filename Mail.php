965b332db5322696d96b421a05e513f8b52c86d0
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-06 22:59:36 +0900

    Doc: Mail.php | PHPDoc > @package > op-core-function

--- Mail.php
+++ Mail.php
@@ -2,6 +2,7 @@
 /**	op-core-function:/Mail.php
  *
  * @created    2025-07-27
+ * @package    op-core-function
  * @author     Tomoaki Nagahara
  * @copyright  Tomoaki Nagahara All right reserved.
  */
